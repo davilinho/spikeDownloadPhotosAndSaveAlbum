eea8c027028d37c3160f0dfe69b718986a22f4d5
davilinho <davilinho@gmail.com>
2025-02-12 09:51:13 +0100

    Initial Commit

---
+++ spikeDownloadPhotosAndSaveAlbum/spikeDownloadPhotosAndSaveAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  spikeDownloadPhotosAndSaveAlbumApp.swift
+//  spikeDownloadPhotosAndSaveAlbum
+//
+//  Created by David Martin Nevado on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct spikeDownloadPhotosAndSaveAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spikeDownloadPhotosAndSaveAlbumTests/spikeDownloadPhotosAndSaveAlbumTests.swift
@@ -0,0 +1,16 @@
+//
+//  spikeDownloadPhotosAndSaveAlbumTests.swift
+//  spikeDownloadPhotosAndSaveAlbumTests
+//
+//  Created by David Martin Nevado on 12/2/25.
+//
+
+import Testing
+
+struct spikeDownloadPhotosAndSaveAlbumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
